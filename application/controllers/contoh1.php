65bac8f34f942e06524708fa1fccfecb6699db01
AndikaCallas <andikaclls12@gmail.com>
2023-09-26 16:41:00 +0700

    penambahan isi contoh1.php

--- application/controllers/contoh1.php
+++ application/controllers/contoh1.php
@@ -4,8 +4,8 @@ class contoh1 extends CI_Controller
     public function index()
     {
         echo "<h1>Perkenalkan</h1>";
-        echo "Nama Saya Andika Callas
-        Saya Tinggal di daerah Bekasi
+        echo "Nama Saya Andika Callas<br>
+        Saya Tinggal di daerah Bekasi<br>
         olahraga yang saya sukai adalah Bulutangkis";
     }
 }
